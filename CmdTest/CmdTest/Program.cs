e8cdca7e493336ed212bd734e95f0f70a6833fa4
GitTest <test@git.com>
2019-04-15 15:20:15 +0800

    push

---
+++ CmdTest/CmdTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CmdTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
