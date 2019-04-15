da712a42dbcb2be576ef4c7804b39dce023fd84f
Cing Ling <lilic6306@outlook.com>
2019-04-15 22:22:35 +0800

    试做Winfrom简易计算器

---
+++ WinfromTest/WinfromDemo/UserControl1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace WinfromDemo
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tNum2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tNum1 = new System.Windows.Forms.TextBox();
+            this.tResult = new System.Windows.Forms.TextBox();
+            this.grop2 = new System.Windows.Forms.GroupBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.btn_less = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.btn_multiply = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.btn_except = new System.Windows.Forms.Button();
+            this.Calculation = new System.Windows.Forms.Button();
+            this.num0 = new System.Windows.Forms.Button();
+            this.decimalPoint = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num1 = new System.Windows.Forms.Button();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.grop2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblOperator);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tNum2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tNum1);
+            this.groupBox1.Controls.Add(this.tResult);
+            this.groupBox1.Location = new System.Drawing.Point(14, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(536, 186);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "显示区域";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(293, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "输入的第二个值:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "输入的第一个值:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(353, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "运算符:";
+            // 
+            // tNum2
+            // 
+            this.tNum2.Location = new System.Drawing.Point(283, 146);
+            this.tNum2.Name = "tNum2";
+            this.tNum2.Size = new System.Drawing.Size(228, 25);
+            this.tNum2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "结果:";
+            // 
+            // tNum1
+            // 
+            this.tNum1.Location = new System.Drawing.Point(6, 146);
+            this.tNum1.Name = "tNum1";
+            this.tNum1.Size = new System.Drawing.Size(225, 25);
+            this.tNum1.TabIndex = 1;
+            // 
+            // tResult
+            // 
+            this.tResult.Location = new System.Drawing.Point(6, 73);
+            this.tResult.Name = "tResult";
+            this.tResult.Size = new System.Drawing.Size(524, 25);
+            this.tResult.TabIndex = 0;
+            // 
+            // grop2
+            // 
+            this.grop2.Controls.Add(this.btn_add);
+            this.grop2.Controls.Add(this.num3);
+            this.grop2.Controls.Add(this.num2);
+            this.grop2.Controls.Add(this.btn_less);
+            this.grop2.Controls.Add(this.num6);
+            this.grop2.Controls.Add(this.num5);
+            this.grop2.Controls.Add(this.btn_multiply);
+            this.grop2.Controls.Add(this.num9);
+            this.grop2.Controls.Add(this.num8);
+            this.grop2.Controls.Add(this.btn_except);
+            this.grop2.Controls.Add(this.Calculation);
+            this.grop2.Controls.Add(this.num0);
+            this.grop2.Controls.Add(this.decimalPoint);
+            this.grop2.Controls.Add(this.num7);
+            this.grop2.Controls.Add(this.num4);
+            this.grop2.Controls.Add(this.num1);
+            this.grop2.Location = new System.Drawing.Point(20, 242);
+            this.grop2.Name = "grop2";
+            this.grop2.Size = new System.Drawing.Size(530, 323);
+            this.grop2.TabIndex = 1;
+            this.grop2.TabStop = false;
+            this.grop2.Text = "输入区域";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(390, 35);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(84, 42);
+            this.btn_add.TabIndex = 0;
+            this.btn_add.Text = "+";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.Btn_add_Click);
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(250, 35);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(84, 42);
+            this.num3.TabIndex = 0;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(141, 35);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(84, 42);
+            this.num2.TabIndex = 0;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            // 
+            // btn_less
+            // 
+            this.btn_less.Location = new System.Drawing.Point(390, 113);
+            this.btn_less.Name = "btn_less";
+            this.btn_less.Size = new System.Drawing.Size(84, 42);
+            this.btn_less.TabIndex = 0;
+            this.btn_less.Text = "-";
+            this.btn_less.UseVisualStyleBackColor = true;
+            this.btn_less.Click += new System.EventHandler(this.Btn_less_Click);
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(250, 113);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(84, 42);
+            this.num6.TabIndex = 0;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(141, 113);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(84, 42);
+            this.num5.TabIndex = 0;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            // 
+            // btn_multiply
+            // 
+            this.btn_multiply.Location = new System.Drawing.Point(390, 191);
+            this.btn_multiply.Name = "btn_multiply";
+            this.btn_multiply.Size = new System.Drawing.Size(84, 42);
+            this.btn_multiply.TabIndex = 0;
+            this.btn_multiply.Text = "*";
+            this.btn_multiply.UseVisualStyleBackColor = true;
+            this.btn_multiply.Click += new System.EventHandler(this.Btn_multiply_Click);
+            // 
+            // num9
+            // 
+            this.num9.Location = new System.Drawing.Point(250, 191);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(84, 42);
+            this.num9.TabIndex = 0;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            // 
+            // num8
+            // 
+            this.num8.Location = new System.Drawing.Point(141, 191);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(84, 42);
+            this.num8.TabIndex = 0;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            // 
+            // btn_except
+            // 
+            this.btn_except.Location = new System.Drawing.Point(390, 258);
+            this.btn_except.Name = "btn_except";
+            this.btn_except.Size = new System.Drawing.Size(84, 42);
+            this.btn_except.TabIndex = 0;
+            this.btn_except.Text = "/";
+            this.btn_except.UseVisualStyleBackColor = true;
+            this.btn_except.Click += new System.EventHandler(this.Btn_except_Click);
+            // 
+            // Calculation
+            // 
+            this.Calculation.Location = new System.Drawing.Point(250, 258);
+            this.Calculation.Name = "Calculation";
+            this.Calculation.Size = new System.Drawing.Size(84, 42);
+            this.Calculation.TabIndex = 0;
+            this.Calculation.Text = "=";
+            this.Calculation.UseVisualStyleBackColor = true;
+            this.Calculation.Click += new System.EventHandler(this.Calculation_Click);
+            // 
+            // num0
+            // 
+            this.num0.Location = new System.Drawing.Point(141, 258);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(84, 42);
+            this.num0.TabIndex = 0;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            // 
+            // decimalPoint
+            // 
+            this.decimalPoint.Location = new System.Drawing.Point(27, 258);
+            this.decimalPoint.Name = "decimalPoint";
+            this.decimalPoint.Size = new System.Drawing.Size(84, 42);
+            this.decimalPoint.TabIndex = 0;
+            this.decimalPoint.Text = ".";
+            this.decimalPoint.UseVisualStyleBackColor = true;
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(27, 191);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(84, 42);
+            this.num7.TabIndex = 0;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(27, 113);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(84, 42);
+            this.num4.TabIndex = 0;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(27, 35);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(84, 42);
+            this.num1.TabIndex = 0;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.AutoSize = true;
+            this.lblOperator.Location = new System.Drawing.Point(420, 40);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(0, 15);
+            this.lblOperator.TabIndex = 5;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.grop2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(572, 580);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.grop2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tNum1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tNum2;
+        private System.Windows.Forms.GroupBox grop2;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button btn_less;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button btn_multiply;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button btn_except;
+        private System.Windows.Forms.Button Calculation;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button decimalPoint;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Label lblOperator;
+    }
+}
---
+++ WinfromTest/WinfromDemo/UserControl1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinfromDemo
+{
+    public partial class UserControl1: UserControl
+    {
+        int actiontype = 0;  //运算符
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// 数字按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnVaL_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (actiontype > 0)
+            {
+                if (!string.IsNullOrEmpty(tNum2.Text.Trim()))
+                {
+                    if (btn.Text == ".")
+                    {
+                        if (tNum2.Text.IndexOf('.') < 0)
+                            tNum2.Text += btn.Text.Trim();
+                        return;
+                    }
+                    tNum2.Text += btn.Text.Trim();
+                }
+                else
+                {
+                    tNum2.Text = btn.Text.Trim();
+                }
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(tNum1.Text.Trim()))
+                {
+                    if (btn.Text == ".")
+                    {
+                        if (tNum1.Text.IndexOf('.') < 0)
+                            tNum1.Text += btn.Text;
+                        return;
+                    }
+                    tNum1.Text += btn.Text;
+                }
+                else
+                {
+                    tNum1.Text = btn.Text;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 绑定事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            num0.Click+= new EventHandler(btnVaL_Click);
+            num1.Click += new EventHandler(btnVaL_Click);
+            num2.Click += new EventHandler(btnVaL_Click);
+            num3.Click += new EventHandler(btnVaL_Click);
+            num4.Click += new EventHandler(btnVaL_Click);
+            num5.Click += new EventHandler(btnVaL_Click);
+            num6.Click += new EventHandler(btnVaL_Click);
+            num7.Click += new EventHandler(btnVaL_Click);
+            num8.Click += new EventHandler(btnVaL_Click);
+            num9.Click += new EventHandler(btnVaL_Click);
+            decimalPoint.Click+= new EventHandler(btnVaL_Click);
+        }
+
+        //加运算
+        private void Btn_add_Click(object sender, EventArgs e)
+        {
+            actiontype = 1;
+            lblOperator.Text = btn_add.Text;
+            if (!string.IsNullOrEmpty(tNum2.Text.Trim()))
+            {
+                tResult.Text = (decimal.Parse(tNum1.Text.Trim()) + decimal.Parse(tNum2.Text.Trim())).ToString();
+            }
+        }
+
+        //减运算
+        private void Btn_less_Click(object sender, EventArgs e)
+        {
+            actiontype = 2;
+            lblOperator.Text = btn_less.Text;
+            if (!string.IsNullOrEmpty(tNum2.Text.Trim()))
+            {
+                tResult.Text = (decimal.Parse(tNum1.Text.Trim()) - decimal.Parse(tNum2.Text.Trim())).ToString();
+            }
+        }
+
+        //乘运算
+        private void Btn_multiply_Click(object sender, EventArgs e)
+        {
+            actiontype = 3;
+            lblOperator.Text = btn_multiply.Text;
+            if (!string.IsNullOrEmpty(tNum2.Text.Trim()))
+            {
+                tResult.Text = (decimal.Parse(tNum1.Text.Trim()) * decimal.Parse(tNum2.Text.Trim())).ToString();
+            }
+        }
+
+        //除运算
+        private void Btn_except_Click(object sender, EventArgs e)
+        {
+            actiontype = 4;
+            lblOperator.Text = btn_except.Text;
+            if (!string.IsNullOrEmpty(tNum2.Text.Trim()))
+            {
+                tResult.Text = (decimal.Parse(tNum1.Text.Trim()) / decimal.Parse(tNum2.Text.Trim())).ToString();
+            }
+        }
+
+        //按钮事件
+        private void Calculation_Click(object sender, EventArgs e)
+        {
+            switch (actiontype) {
+                case 1:
+                    tResult.Text = (decimal.Parse(tNum1.Text.Trim()) + decimal.Parse(tNum2.Text.Trim())).ToString();
+                    break;
+                case 2:
+                    tResult.Text = (decimal.Parse(tNum1.Text.Trim()) - decimal.Parse(tNum2.Text.Trim())).ToString();
+                    break;
+                case 3:
+                    tResult.Text = (decimal.Parse(tNum1.Text.Trim()) * decimal.Parse(tNum2.Text.Trim())).ToString();
+                    break;
+                case 4:
+                    tResult.Text = (decimal.Parse(tNum1.Text.Trim()) / decimal.Parse(tNum2.Text.Trim())).ToString();
+                    break;
+            }
+            actiontype = 0;
+            tNum1.Text = "";
+            tNum2.Text = "";
+        }
+    }
+}
